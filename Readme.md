aae29872d1b9544f257f4c2a8a92dafc97bde781
developer-nieem <naimhossenwork@gmail.com>
2023-04-10 15:09:32 +0600

    aded readme file

---
+++ Readme.md
@@ -0,0 +1,15 @@
+
+## DevJOB Hub features
+
+* We have a good Header with responsive
+* And also have a banner part
+* third section we have dynamic jobs category
+
+ ## most important feature:
+ * We have a jobs feature list you can see the job circular with some details and when you click the view Details you can see the open a new page for all details and you can get a option for apply the jobs
+
+    ## Statistics page 
+    *  You have to show a chart with marks details 
+
+
+### Live link : []()
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
 
 const Blog = () => {
-    return (
-        <div>
-            ff
+  return (
+    <div className="container my-5">
+      <div class="card my-4">
+        <div class="card-header">When should you use context API?</div>
+        <div class="card-body">
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
         </div>
-    );
+      </div>
+      <div class="card my-4">
+        <div class="card-header">What is a custom hook?</div>
+        <div class="card-body">
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+        </div>
+      </div>
+      <div class="card my-4">
+        <div class="card-header">What is useRef?</div>
+        <div class="card-body">
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+        </div>
+      </div>
+      <div class="card my-4">
+        <div class="card-header">What is useMemo?</div>
+        <div class="card-body">
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
