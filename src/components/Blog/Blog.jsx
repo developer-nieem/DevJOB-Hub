54e05f0d64966605ef271d3a848cb8a59a31e865
developer-nieem <naimhossenwork@gmail.com>
2023-04-10 21:57:33 +0600

    added blog QA

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -7,8 +7,7 @@ const Blog = () => {
         <div class="card-header">When should you use context API?</div>
         <div class="card-body">
           <p class="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
+            We can use context api when need data  most of components  , then we declare api a main components on the router and get the data useContext, Example : i declare api data this project on main route then provide all components and catch the data if i need any components 
           </p>
         </div>
       </div>
@@ -16,8 +15,7 @@ const Blog = () => {
         <div class="card-header">What is a custom hook?</div>
         <div class="card-body">
           <p class="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
+           Custom hook  is a beautiful features of react hooks , For custom hooks we can use same functionality and js code many place then we use hooks.  like use useState hooks for state management , its means if we have same functionality on many place then we create custom hooks and use them 
           </p>
         </div>
       </div>
@@ -25,8 +23,7 @@ const Blog = () => {
         <div class="card-header">What is useRef?</div>
         <div class="card-body">
           <p class="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
+          useRef is react hook , That we use for element reference  and keep stored input value. its not changeable 
           </p>
         </div>
       </div>
@@ -34,8 +31,7 @@ const Blog = () => {
         <div class="card-header">What is useMemo?</div>
         <div class="card-body">
           <p class="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
+            useMemo is react hook , that calculation any function and kept memories . and when need any change but useMemo not reRander with give stored value 
           </p>
         </div>
       </div>
